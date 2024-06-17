cb8b54ae02e4bad82254bd9aaf81ad63a6daea15
Ashish Kumar <ashishkumat818@gmail.com>
2024-06-17 21:32:04 +0530

    a

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      Hello {value}
+      <h1>This is my React static web site</h1>
+    </div>
+  );
 }
 
 export default App;
